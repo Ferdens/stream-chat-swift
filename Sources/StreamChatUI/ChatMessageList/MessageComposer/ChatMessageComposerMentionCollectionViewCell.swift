ddb6cf84a4d04d9108968bf93fdf0f00d6f91b74
Dominik Bucher <d.bucher@centrum.cz>
2021-02-13 01:01:28 +0100

    Add documentation to ChatMessageComposerMentionCollectionViewCell

--- Sources/StreamChatUI/ChatMessageList/MessageComposer/ChatMessageComposerMentionCollectionViewCell.swift
+++ Sources/StreamChatUI/ChatMessageList/MessageComposer/ChatMessageComposerMentionCollectionViewCell.swift
@@ -5,26 +5,39 @@
 import StreamChat
 import UIKit
 
+/// A View that is embed inside `UICollectionViewCell`  which shows information about user which we want to tag in suggestions
 public typealias ChatMessageComposerMentionCellView = _ChatMessageComposerMentionCellView<NoExtraData>
 
+/// A View that is embed inside `UICollectionViewCell`  which shows information about user which we want to tag in suggestions
 open class _ChatMessageComposerMentionCellView<ExtraData: ExtraDataTypes>: View, UIConfigProvider {
     // MARK: Properties
 
+    /// Content of the cell
+    /// This is tuple which contains:
+    /// - title: By default name of user
+    /// - subtitle: By default tag of user
+    /// - imageURL: URL of image for the user
+    /// - isUserOnline: Indicates if user is online to set onlineIndicator visible
     open var content: (title: String, subtitle: String, imageURL: URL?, isUserOnline: Bool)? {
         didSet {
             updateContentIfNeeded()
         }
     }
 
+    /// `_ChatChannelAvatarView` instance which holds photo of user for tagging.
     open private(set) lazy var avatarView = uiConfig
         .messageComposer
         .mentionAvatarView
         .init()
         .withoutAutoresizingMaskConstraints
 
+    /// Title label which shows users whole name.
     open private(set) lazy var usernameLabel: UILabel = UILabel().withoutAutoresizingMaskConstraints
+    /// Subtitle label which shows username tag etc. `@user`.
     open private(set) lazy var usernameTagLabel: UILabel = UILabel().withoutAutoresizingMaskConstraints
+    /// ImageView which is located at the right part of the cell, showing @ symbol by default.
     open private(set) lazy var suggestionTypeImageView: UIImageView = UIImageView().withoutAutoresizingMaskConstraints
+    /// StackView which holds username and userTag labels in vertical axis by default.
     private lazy var textStackView: UIStackView = UIStackView().withoutAutoresizingMaskConstraints
 
     // MARK: - Appearance
@@ -53,6 +66,7 @@ open class _ChatMessageComposerMentionCellView<ExtraData: ExtraDataTypes>: View,
         usernameTagLabel.text = content?.subtitle
         usernameLabel.text = content?.title
 
+        // If user has photo, show it. Otherwise use first placeholder.
         if let url = content?.imageURL {
             avatarView.imageView.loadImage(from: url)
         } else {
@@ -80,14 +94,9 @@ open class _ChatMessageComposerMentionCellView<ExtraData: ExtraDataTypes>: View,
         textStackView.addArrangedSubview(usernameLabel)
         textStackView.addArrangedSubview(usernameTagLabel)
         textStackView.centerYAnchor.pin(equalTo: avatarView.centerYAnchor).isActive = true
-        textStackView.leadingAnchor.pin(
-            equalToSystemSpacingAfter: avatarView.trailingAnchor,
-            multiplier: 1
-        ).isActive = true
+        textStackView.leadingAnchor.pin(equalToSystemSpacingAfter: avatarView.trailingAnchor, multiplier: 1).isActive = true
 
-        textStackView.trailingAnchor.pin(
-            equalTo: suggestionTypeImageView.leadingAnchor
-        ).isActive = true
+        textStackView.trailingAnchor.pin(equalTo: suggestionTypeImageView.leadingAnchor).isActive = true
     }
 
     private func setupSuggestionTypeImageViewConstraints() {
@@ -96,27 +105,32 @@ open class _ChatMessageComposerMentionCellView<ExtraData: ExtraDataTypes>: View,
     }
 }
 
+/// `UICollectionView` subclass which embeds inside `ChatMessageComposerMentionCellView`
 public typealias ChatMessageComposerMentionCollectionViewCell = _ChatMessageComposerMentionCollectionViewCell<NoExtraData>
 
+/// `UICollectionView` subclass which embeds inside `ChatMessageComposerMentionCellView`
 open class _ChatMessageComposerMentionCollectionViewCell<ExtraData: ExtraDataTypes>: CollectionViewCell, UIConfigProvider {
     // MARK: Properties
 
+    /// Reuse identifier for the cell used in `collectionView(cellForItem:)`
     open class var reuseId: String { String(describing: self) }
 
+    /// Instance of `ChatMessageComposerMentionCellView` which shows information about the mentioned user.
     public private(set) lazy var mentionView = uiConfig
         .messageComposer
         .suggestionsMentionCellView.init()
         .withoutAutoresizingMaskConstraints
 
+    // MARK: - Lifecycle
+
     override open func setUpLayout() {
         super.setUpLayout()
-        
-        contentView.embed(
-            mentionView,
-            insets: contentView.directionalLayoutMargins
-        )
+        contentView.embed(mentionView, insets: contentView.directionalLayoutMargins)
     }
 
+    // We need this method for `UICollectionViewCells` resize properly inside collectionView
+    // and respect collectionView width. Without this method, the collectionViewCell content
+    // autoresizes itself and ignores bounds of parent collectionView
     override open func preferredLayoutAttributesFitting(
         _ layoutAttributes: UICollectionViewLayoutAttributes
     ) -> UICollectionViewLayoutAttributes {
